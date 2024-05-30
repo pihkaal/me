af5fc8cb3555a86fb344e9936692f44c9b6e1363
Pihkaal <pihkaal@proton.me>
2024-05-30 15:53:44 +0200

    chore(prettier): update config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,18 +1,8 @@
 // @ts-check
 
-/** @type {import("prettier").Options & import("prettier-plugin-tailwindcss").PluginOptions} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
-  arrowParens: "avoid",
-  bracketSpacing: true,
-  quoteProps: "consistent",
-  jsxSingleQuote: false,
-  printWidth: 80,
-  semi: true,
-  singleQuote: false,
-  tabWidth: 2,
-  trailingComma: "all",
-  useTabs: false,
 };
 
 module.exports = config;
