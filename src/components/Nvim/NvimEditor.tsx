68b483f89cd1615fe0945a9f6d5b9ab23454505b
Pihkaal <pihkaal@proton.me>
2024-06-01 00:33:04 +0200

    feat(nvim-editor): fetch and display opened file

--- src/components/Nvim/NvimEditor.tsx
+++ src/components/Nvim/NvimEditor.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export const NvimEditor = (props: { source: string | undefined }) => {
   const [data, setData] = useState<string>();
   const [loading, setLoading] = useState(false);
 
-  return <div className="h-full">{props.source}</div>;
+  useEffect(() => {
+    if (!props.source) return;
+    setLoading(true);
+    axios.get<string>(props.source).then(({ data }) => {
+      setData(data);
+      setLoading(false);
+    });
+  }, [props.source]);
+
+  return <div className="h-full">{loading ? "Loading..." : data}</div>;
 };
