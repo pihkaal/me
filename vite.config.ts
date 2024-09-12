62731fb608819ea01c759356f4376a5f5f2db285
Pihkaal <pihkaal@proton.me>
2024-09-12 17:15:12 +0200

    feat: remove useless dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
-import rollupNodePolyfillsPlugin from "rollup-plugin-polyfill-node";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 import { manifest } from "./build/manifestPlugin";
 
 const config = defineConfig({
-  plugins: [manifest(), tsconfigPaths(), react(), rollupNodePolyfillsPlugin()],
-  resolve: {
-    alias: {
-      events: "rollup-plugin-node-polyfills/polyfills/events",
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({ buffer: true, process: true }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
+  plugins: [react(), manifest(), tsconfigPaths()],
 });
 
 export default config;
