7b9c438f02434f53095ede20cafd92819d31af4d
Pihkaal <pihkaal@proton.me>
2024-09-13 14:22:56 +0200

    chore: remove dead code

--- src/components/Nvim/NvimEditor.tsx
+++ src/components/Nvim/NvimEditor.tsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
 export const NvimEditor = (props: { content: string | undefined }) => {
-  const [selectedLine, setSelectedLine] = useState(0);
-
   let rows = props.content?.split("\n") ?? [];
   // trim end empty lines
   for (let i = rows.length - 1; i >= 0; i--) {
@@ -26,32 +22,5 @@ export const NvimEditor = (props: { content: string | undefined }) => {
         dangerouslySetInnerHTML={{ __html: props.content ?? "" }}
       />
     </div>
-  );
-
-  return (
-    <table>
-      <tbody>
-        {rows.map((row, i) => (
-          <tr
-            key={i}
-            onMouseDown={() => setSelectedLine(i)}
-            onMouseMove={(e) => {
-              if (e.buttons === 1) setSelectedLine(i);
-            }}
-          >
-            <td
-              className={`flex select-none flex-col items-start pl-[2ch] pr-[1ch] text-right`}
-              style={{
-                width: `${3 + rows.length.toString().length}ch`,
-                color: selectedLine === i ? "inherit" : "#6b616d",
-              }}
-            >
-              {i + 1}
-            </td>
-            <td>{row}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
   );
 };
